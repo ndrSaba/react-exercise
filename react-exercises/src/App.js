f43e788abd1f5504e3ee56af0918b615f0619205
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-08 15:36:18 +0200

    es fatto

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import {Hello} from "./Hello"; 
-
-const root = document.querySelector('#root')
 
 export class App extends React.Component {
     render() {
         return <div><Hello /></div>
     }
 }
-
-ReactDOM.render(<App />, root)
--- react-exercises/src/Hello.js
+++ react-exercises/src/Hello.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class Hello extends React.Component {
+export class Hello extends React.Component {
     render() {
         return <h1>Hello, World!</h1>
     }
