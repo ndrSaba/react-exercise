5b774331b521b72ff5776ff2660bfc6599e58c22
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-10 13:44:06 +0200

    es fatto

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
-import {Hello} from "./Hello"; 
+import { Hello } from "./Hello";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
     render() {
-        return <div><Hello /></div>
+        return (
+            <div>
+                <Hello />
+                <Welcome name="Andrea" />
+            </div>
+        )
     }
 }
