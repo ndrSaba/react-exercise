d5abc981c7906d9841da924f73da1f2e2dce08ea
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-10 13:51:16 +0200

    es fixato

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Hello />
-                <Welcome />
+                <Welcome name="Andrea" />
             </div>
         )
     }
