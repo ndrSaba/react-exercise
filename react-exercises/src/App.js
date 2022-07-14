fbfcb1c119a12027b302ab80d8f0b576d307dca9
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-14 17:27:52 +0200

    es fatto

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Hello />
-                <Welcome name={<strong>Andrea</strong>} age/>
+                <Welcome name='John' age={28}/>
             </div>
         )
     }
--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Age } from "./Age";
+import { Age } from "./Age"
 
 export class Welcome extends React.Component {
     render() {
         return (
             <div>
                 <p>Welcome, {this.props.name}!</p>
-                {this.props.age && <p><Age age = {28}/></p>}
+                <p>{this.props.age >= 18 && this.props.age <= 65 && this.props.name === 'John'
+                    ? <Age age={this.props.age} />
+                    : <></>}</p>
             </div>
         )
     }
