542599f93265c735203b92f2891aebda7a00542e
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-15 12:20:04 +0200

    es fatto

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Counter } from "./Counter";
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
 
@@ -8,6 +9,7 @@ export class App extends React.Component {
             <div>
                 <Hello />
                 <Welcome name='John' age={28}/>
+                <Counter initialValue={0} incrementBy={1} interval={1000}/>
             </div>
         )
     }
---
+++ react-exercises/src/Counter.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export class Counter extends React.Component {
+    state = {
+        count: this.props.initialValue ?? 0
+    }
+
+    constructor(props) {
+        super(props)
+
+        setInterval(() => {
+            this.setState((state) => {
+                return {
+                    count: state.count + (this.props.incrementBy ?? 1)
+                }
+            })
+        }, this.props.interval ?? 1000)
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+            </div>
+        )
+    }
+}
+
