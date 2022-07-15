a092a3023ce019989b7fd9fc9ddd9d474963e09c
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-15 11:22:35 +0200

    es fatto

--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -6,9 +6,10 @@ export class Welcome extends React.Component {
         return (
             <div>
                 <p>Welcome, {this.props.name}!</p>
-                <p>{this.props.age >= 18 && this.props.age <= 65 && this.props.name === 'John'
+                {this.props.age >= 18 && this.props.age <= 65 && this.props.name === 'John'
                     ? <Age age={this.props.age} />
-                    : <></>}</p>
+                    : <></>}
+                {this.props.age < 18 && <p>You are very young!</p>}
             </div>
         )
     }
