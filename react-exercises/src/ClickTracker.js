4eb244db7f74841865c8252387b5c4f5fe88f80c
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-19 12:33:43 +0200

    es fatto

---
+++ react-exercises/src/ClickTracker.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export class ClickTracker extends React.Component {
+
+    eventHandler = (event) => {
+        console.log(
+            event.target.name,
+            event.button
+        )
+    }
+
+    render() {
+        return (<div>
+            <button name="button1" onClick={this.eventHandler}>Button 1</button>
+            <button name="button2" onClick={this.eventHandler}>Button 2</button>
+            <button name="button3" onClick={this.eventHandler}>Button 3</button>
+        </div>)
+    }
+}
