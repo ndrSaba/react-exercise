ce7dae30905dc9957f980bd19c32a387f92d732c
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-27 13:33:36 +0200

    es fatto

--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { Age } from "./Age"
 
-export class Welcome extends React.Component {
-    render() {
-        return (
-            <div className="welcome">
-                <p>Welcome, {this.props.name}!</p>
-                {this.props.age >= 18 && this.props.age <= 65 && this.props.name === 'John'
-                    ? <Age age={this.props.age} />
-                    : <></>}
-                {this.props.age < 18 && <p>You are very young!</p>}
-            </div>
-        )
-    }
+export function Welcome(props) {
+    return (
+        <div className="welcome">
+            <p>Welcome, {props.name}!</p>
+            <p>{props.age >= 18 & props.age <= 65 & props.name === 'John'
+                ? <Age age={props.age} />
+                : <></>}
+                {props.age < 18 && <span>You are very young!</span>}</p>
+        </div>
+    )
 }
+
+// export class Welcome extends React.Component {
+//     render() {
+//         return (
+//             <div className="welcome">
+//                 <p>Welcome, {this.props.name}!</p>
+//                 {this.props.age >= 18 && this.props.age <= 65 && this.props.name === 'John'
+//                     ? <Age age={this.props.age} />
+//                     : <></>}
+//                 {this.props.age < 18 && <p>You are very young!</p>}
+//             </div>
+//         )
+//     }
+// }
 
 Welcome.defaultProps = {
     name: "stranger",
