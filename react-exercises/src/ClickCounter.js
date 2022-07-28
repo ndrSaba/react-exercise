9739450ebec1efbc743fcffd8fcbd9b710f322fa
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-07-28 15:45:11 +0200

    es fatto

--- react-exercises/src/ClickCounter.js
+++ react-exercises/src/ClickCounter.js
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export function ClickCounter({ initialValue = 0 }) {
     const [count, setCount] = useState(initialValue)
+
+    useEffect(() => {
+        console.log(`The current value of the counter is ${count}`)
+    })
 
     function incrementCounterOnClick() {
         setCount(c => c + 1)
