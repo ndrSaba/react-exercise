737cf8373c573beec67c0ad9aa93f584eabeff7e
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-08-22 16:18:23 +0200

    es fatto

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,72 +1,10 @@
-import React, { useState } from "react";
-import { UncontrolledLogin } from "./UncontrolledLogin";
-import { ClickCounter } from "./ClickCounter";
-import { ClickTracker } from "./ClickTracker";
-import { Counter } from "./Counter";
-import { Hello } from "./Hello";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./Login";
 import { Welcome } from "./Welcome";
-import { TodoList } from "./TodoList";
-import { Container } from "./Container";
-import { DisplayLanguage } from "./DisplayLanguage";
-import { Sum } from "./Sum";
-import { GithubUser } from "./GithubUser";
-import { GithubUserList } from "./GithubUserList";
-import { CarDetails } from "./CarDetails";
-import { LanguageContext } from "./LanguageContext";
+import { Routes, Route } from "react-router-dom";
 
 export function App() {
-    const [language, setLanguage] = useState("en")
-
-    function handleChangeLanguage(event) {
-        setLanguage(event.target.value)
-    }
-
     return (
-        <React.Fragment>
-            <select onChange={handleChangeLanguage}>
-                <option value="en">English</option>
-                <option value="it">Italiano</option>
-            </select>
-            <LanguageContext.Provider value={language}>
-                <Container title="My Destroyed Application">
-                    
-                    <div>
-                        <DisplayLanguage />
-                        <Hello />
-                        <Welcome name='John' age={28} />
-                        <Counter />
-                        <ClickCounter />
-                        <ClickTracker />
-                        <InteractiveWelcome />
-                        <Login />
-                        <UncontrolledLogin />
-                        <Sum />
-                        <TodoList render={(stringsArr) => {
-                            return (
-                                <div>
-                                    <ul>
-                                        {stringsArr.map((string, index) => (
-                                            <li key={string + index}>{string}
-                                                <button onClick={() => {
-                                                    stringsArr.splice(index, 1);
-                                                    this.setState({
-                                                        stringsArr: stringsArr
-                                                    })
-                                                }}>Remove String</button>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            )
-                        }} />
-                        <GithubUser username="ndrSaba" />
-                        <GithubUserList />
-                        <CarDetails />
-                    </div>
-                </Container >
-            </LanguageContext.Provider>
-        </React.Fragment >
+        <Routes>
+           <Route path="/" element={<Welcome name='John' age={28}/>} />
+        </Routes>
     )
 }
--- react-exercises/src/index.js
+++ react-exercises/src/index.js
@@ -1,9 +1,13 @@
-import {createRoot} from 'react-dom/client';
-import { App } from './App';
+import { createRoot } from 'react-dom/client';
 import './index.css';
+import { Root } from './Root';
+import React from 'react';
 
-
-const container = document.querySelector('#root');
+const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+    <React.Fragment>
+        <Root />
+    </React.Fragment>
+    );
