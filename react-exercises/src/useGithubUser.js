2fe7ba9d83a9bc7352995945a37fc6092ac0f4df
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-08-23 15:52:45 +0200

    es fatto

--- react-exercises/src/useGithubUser.js
+++ react-exercises/src/useGithubUser.js
@@ -3,10 +3,8 @@ import useSWR from "swr";
 const fetcher = url => fetch(url).then(response => response.json());
 
 export function useGithubUser(username){
-    const {data, error, mutate} = useSWR(`https://api.github.com/users/${username}`, fetcher);
-
+    const {data, error, mutate} = useSWR(username ? `https://api.github.com/users/${username}`: null, fetcher);
     
-
     return{
         data,
         error,
