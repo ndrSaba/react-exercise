d1d4049b1fd48273296e0a5419af5d471faff200
ndrSaba <102291579+ndrSaba@users.noreply.github.com>
2022-08-23 14:01:42 +0200

    es fatto

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -10,12 +10,14 @@ export function App() {
     return (
         <Container title={<Hello />}>
             <div>
-                <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="users/:username">GitHub User</Link>
+                <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="users/:username">GitHub User</Link> | <Link to="/testPage">Not Found Test Page</Link>
             </div>
             <Routes>
                 <Route path="/" element={<Welcome name='John' age={28} />} />
                 <Route path="/counter" element={<Counter />} />
-                <Route path="users/:username" element={<ShowGitHubUser username={"ndrSaba"}/>} />
+                <Route path="users/:username" element={<ShowGitHubUser username={"ndrSaba"} />} />
+                <Route path="*" element={<div><p>Not Found</p>
+                    <Link to="/">Go Home</Link></div>} />
             </Routes>
         </Container>
     )
